16f794549470a281174654f647b6bd5bb928c6cd
Jhun Mark Romano <jhunmarkromano2@gmail.com>
2026-05-19 18:49:11 +0800

    fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,8 +22,10 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
-      <Analytics />
+      <body className="min-h-full flex flex-col">{children}
+        <Analytics />
+      </body>
+      
     </html>
   );
 }
